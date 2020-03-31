240114e287e635efe793ff4b363a8e58b5824921
BN0v0 <bnovo1997@gmail.com>
2020-03-31 19:20:30 +0100

    Secound commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// Gloabal variables
 const selectable_area = document.querySelector(".bus")
 var selected = 0;
+var validate=false;
 var total_selected=document.getElementById("quantity").value;
 var seats = document.getElementsByClassName('seat');
 var seats_booked = document.getElementsByClassName('seat-booked');
@@ -10,18 +12,21 @@ selectable_area.addEventListener('click', function(e){
 
     validateSelection();
 
-    if(selected !== parseInt(total_selected)){
-        if(e.target.classList.contains('seat') && !e.target.classList.contains('seat-booked')){
-            e.target.classList.toggle('seat-selected');
-            selected++;
+    if(validate ==false){
+        if(selected !== parseInt(total_selected) ){
+            if(e.target.classList.contains('seat') && !e.target.classList.contains('seat-booked')){
+                e.target.classList.toggle('seat-selected');
+                selected++;
+            }
+        }else{
+            if(e.target.classList.contains('seat-selected') && e.target.classList.contains('seat')){
+                e.target.classList.toggle('seat-selected');
+                selected--;
+            }
         }
     }else{
-        if(e.target.classList.contains('seat-selected') && e.target.classList.contains('seat')){
-            e.target.classList.toggle('seat-selected');
-            selected--;
-        }
+        validate =false;
     }
-    
 
 })
 
@@ -29,9 +34,18 @@ selectable_area.addEventListener('click', function(e){
 function validateSelection(){
     var total_selected=document.getElementById("quantity").value;
 
-    if(parseInt(total_selected) <selected){
-        seleced = 0; 
-        document.reload();
+    if(selected >= parseInt(total_selected)){
+        selected = 0; 
+        var list= selectable_area.getElementsByClassName("seat");
+
+        for(var i = 0 ; i< list.length ; i++){
+            if ( list[i].classList.contains('seat-selected') ){
+                list[i].classList.toggle('seat-selected');
+            }
+        }
+
+        validate = true;
+        
     }
 }
 
